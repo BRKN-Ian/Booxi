aaa0218d3f6c1b840bc52388b7d8f6a54dc43832
BRKN-Ian <ianfblack@gmail.com>
2025-08-03 21:23:54 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "2rem" }}>
+      <h1>Booxi Scheduler</h1>
+      <p>You're seeing this because your React frontend is working!</p>
+    </div>
+  );
+}
